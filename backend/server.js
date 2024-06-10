33b7dbc92f6a546e368b48a22c2fc8ecd602aff6
lucasmsardi <lucasmsardi@gmail.com>
2024-06-10 17:39:18 -0300

    adicionei o header pra não tomar rate limiting

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,12 @@ app.get('/api/scrape', async (req, res) => {
   console.log(`Buscando por: ${keyword}`);
 
   try {
-    const response = await axios.get(url);
+    const response = await axios.get(url, {
+      headers: {
+        'User-Agent':
+          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+      },
+    });
     const data = {
       titles: scrapeTitles(response),
       numberReviews:
